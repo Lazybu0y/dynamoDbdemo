ac535b9786ea59e9b1e25a5a52dc4055927e7873
Santosh Kumar Singh <chat.wid.santosh@gmail.com>
2022-05-11 10:17:50 -0500

    Added config file.

--- src/main/java/com/lazybu0y/Main.java
+++ src/main/java/com/lazybu0y/Main.java
@@ -9,26 +9,50 @@ import com.lazybu0y.entities.RepoScanResult;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.nio.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000","US_EAST_1"))
-                .build();
-
-        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-        RepoScanResult repoScanResultSchema = new RepoScanResult();
-        //repoScanResultSchema.setScanId("A");
-        FileScanResult fileScanResult = new FileScanResult();
-        fileScanResult.setFileName("application.json");
-        fileScanResult.setLineNum("102");
-        fileScanResult.setSuspiciousKey("65r%123%iuyh");
-        repoScanResultSchema.setRepoName("ABCRepo");
-        repoScanResultSchema.setFileScanResult(fileScanResult);
-        dynamoDBMapper.save(repoScanResultSchema);
+        loadProperty();
+//        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+//                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000","US_EAST_1"))
+//                .build();
+//
+//        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+//        RepoScanResult repoScanResultSchema = new RepoScanResult();
+//        //repoScanResultSchema.setScanId("A");
+//        FileScanResult fileScanResult = new FileScanResult();
+//        fileScanResult.setFileName("application.json");
+//        fileScanResult.setLineNum("102");
+//        fileScanResult.setSuspiciousKey("65r%123%iuyh");
+//        repoScanResultSchema.setRepoName("ABCRepo");
+//        repoScanResultSchema.setFileScanResult(fileScanResult);
+//        dynamoDBMapper.save(repoScanResultSchema);
         //RepoScanResult result = dynamoDBMapper.load(RepoScanResult.class,repoScanResultSchema.getScanId(),repoScanResultSchema.getRepoName());
+    }
+
+    private static void loadProperty() {
+        Properties defaultProperties = new Properties();
+       // Path path = Paths.get("/Users/santoshsingh/Projects/DynamoDbAndSpringIntegration/dynamoDbdemo/src/main/resources/config.properties");
+        Path path = Paths.get("config.properties");
+        System.out.println(path.toAbsolutePath());
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path.getFileName())){
+            System.out.println(bufferedReader.readLine());
+            defaultProperties.load(bufferedReader);
+            System.out.println(defaultProperties.entrySet().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
